77993c57ee6bfb08178949371e6c4a66522f910f
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2024-02-28 14:21:50 +0530

    profile update and Admin role changing and also added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -168,3 +168,87 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res);
 });
+
+// Update User Profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData={
+        name:req.body.name,
+        email:req.body.email,
+    }
+
+    // We cloudinary later
+
+    const user = await User.findByIdAndUpdate(req.user.id,newUserData,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false,
+    })
+    res.status(200).json({
+        success:true,
+    })
+});
+
+// Get all User(admin)
+exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
+    
+    const users = await User.find()
+
+    res.status(200).json({
+        success:true,
+        users,
+    })
+})
+
+// Get single User(admin)
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+    
+    const user = await User.findById(req.params.id)
+
+    if(!user){
+        return next(new ErrorHandler(`User does not exist with Id: ${req.params.id}`))
+    }
+
+    res.status(200).json({
+        success:true,
+        user,
+    })
+})
+
+// Update User Role
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+    const newUserData={
+        name:req.body.name,
+        email:req.body.email,
+        role: req.body.role,
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id,newUserData,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false,
+    })
+
+    if(!user){
+        return next(new ErrorHandler(`User does not exist with Id: ${req.params.id}`))
+    }
+    res.status(200).json({
+        success:true,
+    })
+});
+
+// Delete User
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+    
+    const user = await User.findById(req.params.id)
+
+    if(!user){
+        return next(new ErrorHandler(`User does not exist with Id: ${req.params.id}`))
+    }
+
+    await user.remove();
+
+
+    res.status(200).json({
+        success:true,
+    })
+});
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,10 +6,16 @@ const router = express.Router();
 
 // Routes
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),isAuthenticatedUser,createProduct);
-router.route("/product/:id")
-.put(isAuthenticatedUser,authorizeRoles("admin"), updateProduct)
-.delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
-.get(getProductDetails)
+
+router
+    .route("/product/new")
+    .post(isAuthenticatedUser,authorizeRoles("admin"),isAuthenticatedUser,createProduct);
+
+router
+    .route("/admin/product/:id")
+    .put(isAuthenticatedUser,authorizeRoles("admin"), updateProduct)
+    .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
+    
+    router.route("/product/:id").get(getProductDetails);
 
 module.exports  = router
